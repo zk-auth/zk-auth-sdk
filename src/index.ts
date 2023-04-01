fcb0494dc0035645895ddbacdb62f475de41ec88
Manu López <manulpz41@gmail.com>
2023-04-01 18:00:15 -0300

    a

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,115 @@
-import express from 'express'
-const {auth, resolver, loaders} = require('@iden3/js-iden3-auth')
-const getRawBody = require('raw-body')
+import express from "express";
+import { auth, resolver, loaders } from "@iden3/js-iden3-auth";
+import getRawBody from "raw-body";
+import { VerifyOpts } from "@iden3/js-iden3-auth/dist/cjs/circuits";
 
 const app = express();
 const port = 8000;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 app.get("/api/sign-in", (req, res) => {
-    console.log('get Auth Request');
-    GetAuthRequest(req, res);
+  console.log("get Auth Request");
+  GetAuthRequest(req, res);
 });
 
 app.post("/api/callback", (req, res) => {
-    console.log('callback');
-    Callback(req,res);
+  console.log("callback");
+  Callback(req, res);
 });
 
 app.listen(port, () => {
-    console.log('server running on port 8080');
+  console.log("server running on port 8080");
 });
 
 // Create a map to store the auth requests and their session IDs
 const requestMap = new Map();
 
 async function GetAuthRequest(req: express.Request, res: express.Response) {
+  // Audience is verifier id
+  const hostUrl = "<NGROK_URL>";
+  const sessionId = 1;
+  const callbackURL = "/api/callback";
+  const audience =
+    "did:polygonid:polygon:mumbai:2qDyy1kEo2AYcP3RT4XGea7BtxsY285szg6yP9SPrs";
 
-    // Audience is verifier id
-    const hostUrl = "<NGROK_URL>";
-    const sessionId = 1;
-    const callbackURL = "/api/callback"
-    const audience = "did:polygonid:polygon:mumbai:2qDyy1kEo2AYcP3RT4XGea7BtxsY285szg6yP9SPrs"
+  const uri = `${hostUrl}${callbackURL}?sessionId=${sessionId}`;
 
-    const uri = `${hostUrl}${callbackURL}?sessionId=${sessionId}`;
+  // Generate request for basic authentication
+  const request = auth.createAuthorizationRequest("test flow", audience, uri);
 
-    // Generate request for basic authentication
-    const request = auth.createAuthorizationRequest(
-        'test flow',
-        audience,
-        uri,
-    );
+  request.id = "7f38a193-0918-4a48-9fac-36adfdb8b542";
+  request.thid = "7f38a193-0918-4a48-9fac-36adfdb8b542";
 
-    request.id = '7f38a193-0918-4a48-9fac-36adfdb8b542';
-    request.thid = '7f38a193-0918-4a48-9fac-36adfdb8b542';
-
-    // Add request for a specific proof
-    const proofRequest = {
-        id: 1,
-        circuitId: 'credentialAtomicQuerySigV2',
-        query: {
-          allowedIssuers: ['*'],
-          type: 'KYCAgeCredential',
-          context: 'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld',
-          credentialSubject: {
-            birthday: {
-              $lt: 20000101,
-            },
-          },
+  // Add request for a specific proof
+  const proofRequest = {
+    id: 1,
+    circuitId: "credentialAtomicQuerySigV2",
+    query: {
+      allowedIssuers: ["*"],
+      type: "KYCAgeCredential",
+      context:
+        "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
+      credentialSubject: {
+        birthday: {
+          $lt: 20000101,
+        },
       },
-      };
-    const scope = request.body.scope ?? [];
-    request.body.scope = [...scope, proofRequest];
+    },
+  };
+  const scope = request.body.scope ?? [];
+  request.body.scope = [...scope, proofRequest];
 
-    // Store auth request in map associated with session ID
-    requestMap.set(`${sessionId}`, request);
+  // Store auth request in map associated with session ID
+  requestMap.set(`${sessionId}`, request);
 
-    return res.status(200).set('Content-Type', 'application/json').send(request);
+  return res.status(200).set("Content-Type", "application/json").send(request);
 }
 
 async function Callback(req: express.Request, res: express.Response) {
+  // Get session ID from request
+  const sessionId = req.query.sessionId;
 
-    // Get session ID from request
-    const sessionId = req.query.sessionId;
+  // get JWZ token params from the post request
+  const raw = await getRawBody(req);
+  const tokenStr = raw.toString().trim();
 
-    // get JWZ token params from the post request
-    const raw = await getRawBody(req);
-    const tokenStr = raw.toString().trim();
+  const ethURL = "<MUMBAI_RPC_URL>";
+  const contractAddress = "0x134B1BE34911E39A8397ec6289782989729807a4";
+  const keyDIR = "../keys";
 
-    const ethURL = '<MUMBAI_RPC_URL>';
-    const contractAddress = "0x134B1BE34911E39A8397ec6289782989729807a4"
-    const keyDIR = "../keys"
+  const ethStateResolver = new resolver.EthStateResolver(
+    ethURL,
+    contractAddress
+  );
 
-    const ethStateResolver = new resolver.EthStateResolver(
-        ethURL,
-        contractAddress,
-      );
+  const resolvers = {
+    ["polygon:mumbai"]: ethStateResolver,
+  };
 
-    const resolvers = {
-        ['polygon:mumbai']: ethStateResolver,
+  // fetch authRequest from sessionID
+  const authRequest = requestMap.get(`${sessionId}`);
+
+  // Locate the directory that contains circuit's verification keys
+  const verificationKeyloader = new loaders.FSKeyLoader(keyDIR);
+  const sLoader = new loaders.UniversalSchemaLoader("ipfs.io");
+
+  // EXECUTE VERIFICATION
+  const verifier = new auth.Verifier(verificationKeyloader, sLoader, resolvers);
+
+  let authResponse;
+
+  try {
+    const opts: VerifyOpts = {
+      acceptedStateTransitionDelay: 5 * 60 * 1000, // 5 minute
     };
-
-
-    // fetch authRequest from sessionID
-    const authRequest = requestMap.get(`${sessionId}`);
-
-    // Locate the directory that contains circuit's verification keys
-    const verificationKeyloader = new loaders.FSKeyLoader(keyDIR);
-    const sLoader = new loaders.UniversalSchemaLoader('ipfs.io');
-
-    // EXECUTE VERIFICATION
-    const verifier = new auth.Verifier(
-    verificationKeyloader,
-    sLoader,
-    resolvers,
-    );
-
-
-    try {
-        const opts = {
-            AcceptedStateTransitionDelay: 5 * 60 * 1000, // 5 minute
-        };        
-        authResponse = await verifier.fullVerify(tokenStr, authRequest, opts);
-        
-    } catch (error) {
-
+    authResponse = await verifier.fullVerify(tokenStr, authRequest, opts);
+  } catch (error) {
     return res.status(500).send(error);
+  }
 
-    }
-
-    return res.status(200).set('Content-Type', 'application/json').send("user with ID: " + authResponse.from + " Succesfully authenticated");
+  return res
+    .status(200)
+    .set("Content-Type", "application/json")
+    .send("user with ID: " + authResponse.from + " Succesfully authenticated");
 }
