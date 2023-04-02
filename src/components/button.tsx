ecb1a35422331519f3689f1e77ee3bbd861b65d5
Manu López <manulpz41@gmail.com>
2023-04-02 08:12:06 -0300

    Some fixes

---
+++ src/components/button.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import QRCode from "react-qr-code";
+
+import ZkAuthLogo from "./logo";
+
+export type SignInWithZkAuthButtonProps = {
+  onSuccess: string;
+  onError?: string;
+};
+
+const BASE_URL = window.location.origin + window.location.pathname;
+
+const styles = {
+  fontFamily: "Nunito",
+  border: "none",
+  display: "flex",
+  justifyContent: "center",
+  gap: "10px",
+  alignItems: "center",
+  height: "40px",
+  borderRadius: "20px",
+  padding: "0 20px",
+  background: "black",
+};
+
+const SignInWithZkAuthButton = ({
+  onSuccess,
+  onError,
+}: SignInWithZkAuthButtonProps) => {
+  const [value, setValue] = React.useState<string>("");
+
+  const handlerOnClick = () => {
+    fetch(`${BASE_URL}/api/sign-in`)
+      .then((r) =>
+        Promise.all([Promise.resolve(r.headers.get("x-id")), r.json()])
+      )
+      .then(([id, data]) => {
+        console.log(data);
+        setValue(data);
+        return id;
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      {value == "" ? (
+        <button onClick={handlerOnClick} className={`${styles}`} style={styles}>
+          <ZkAuthLogo />
+          Sign in with ZK Auth
+        </button>
+      ) : (
+        <div
+          style={{
+            height: "auto",
+            margin: "0 auto",
+            maxWidth: 64,
+            width: "100%",
+          }}
+        >
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+            value={value}
+            viewBox={`0 0 256 256`}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SignInWithZkAuthButton;
