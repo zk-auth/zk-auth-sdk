df96f77abf0e4a9b922dd7459565e3c02bbcd497
Manu López <manulpz41@gmail.com>
2023-04-02 08:20:57 -0300

    Upload build

---
+++ dist/components/button.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+export declare type SignInWithZkAuthButtonProps = {
+    onSuccess: string;
+    onError?: string;
+};
+declare const SignInWithZkAuthButton: ({ onSuccess, onError, }: SignInWithZkAuthButtonProps) => JSX.Element;
+export default SignInWithZkAuthButton;
---
+++ dist/components/logo.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+export declare type ZkAuthLogoProps = {
+    color?: string;
+};
+declare const ZkAuthLogo: ({ color }: ZkAuthLogoProps) => JSX.Element;
+export default ZkAuthLogo;
